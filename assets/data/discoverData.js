f26d45332ba9762a0a79d274777815d8705e3bca
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-21 20:20:13 +0000

    Create discoverData.js

---
+++ assets/data/discoverData.js
@@ -0,0 +1,43 @@
+const discoverData = [
+  {
+    id: "discover-1",
+    title: "Lounging at the Labadi Beach",
+    location: 'Ghana',
+    image: require("../images/labadi.jpg"),
+    imageBig: require("../images/labadi.jpg"),
+    description:
+      "Labadi Beach or more properly known as La Pleasure Beach is the busiest beach on Ghana's coast. It is one of Accra's beaches and is maintained by the local hotels. Labadi Beach is in a town called La, popularly known as Labadi.",
+    liked: true,
+    price: 5,
+    rating: 5,
+    duration: 2,
+  },
+  {
+    id: "discover-2",
+    title: "Hiking the Grand Canyon",
+    location: "USA",
+    image: require("../images/canyon.png"),
+    imageBig: require("../images/canyon.png"),
+    description:
+      "Great day hikes and backpacking routes on the North and South Rim of this century-old national park include easy hikes overlooking the rim and more rugged trekking options that descend into the canyon.",
+    liked: true,
+    price: 350,
+    rating: 4.5,
+    duration: 3,
+  },
+  {
+    id: "discover-3",
+    title: "Kayaking in the Tofino Sea",
+    location: 'Canada',
+    image: require("../images/kayak.png"),
+    imageBig: require("../images/kayak.png"),
+    description:
+      "An ideal introduction to sea kayaking around the stunning historical Islands of Tofino's harbour. Come explore the spectacular scenery of the area and learn what makes the area so fascinating.",
+    liked: true,
+    price: 50,
+    rating: 4.7,
+    duration: 2,
+  },
+];
+
+export default discoverData;
